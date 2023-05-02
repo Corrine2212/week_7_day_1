b04d1b73dc6258f2c1b88287403e44d48045b289
Corrine Sing <corrine.sing@gmail.com>
2023-05-02 07:19:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  // for mvp:
+  // const [items, setItems] = useState([
+  //   "Buy shopping", "Clean bathroom", "Car's MOT"
+  // ]);
+
+  // for extension:
+  // setting item state variables with array of objects
+  const [items, setItems] = useState([
+    {name: "Buy shopping", priority: "high" },
+    {name: "Clean bathroom", priority: "low" },
+    {name: "Car's MOT", priority: "high" },
+  ])
+  
+  // state variables that hold name and priority
+  const[newItemName, setNewItemName] = useState("")
+  const[newItemPriority, setNewItemPriority] = useState("")
+  
+  // mapping the 
+  const itemNodes = items.map((item, index) => {
+    return(
+      <li key={index} className={item.priority === "high" ? "high" : "low"}>
+        <span>{item.name}</span>
+        <span>{item.priority}</span>
+      </li>
+    )
+  })
+
+  const handleItemInput = (event) => {
+    setNewItemName(event.target.value)
+  }
+
+  const handlePriorityChange = (event) => {
+    setNewItemPriority(event.target.value)
+    
+  }
+
+  const saveNewItem = (event) => {
+    event.preventDefault();
+    const copyItems = [...items];
+    copyItems.push({name: newItemName, priority: newItemPriority});
+    setItems(copyItems);
+    setNewItemName("")
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>To Do</h1>
+      <hr></hr>
+
+      <form onSubmit={saveNewItem}>
+        <input id="new-item" type="text" value={newItemName} onChange={handleItemInput}></input>
+        <label htmlFor="new-item-high">High</label>
+        <input id="new-item-high" name="priority" type="radio" value="high" onChange={handlePriorityChange}></input>
+        <label htmlFor="new-item-low">Low</label>
+        <input id="new-item-low" name="priority" type="radio" value="low" onChange={handlePriorityChange}></input>
+        <input type="submit" value="Save Item"/>
+      </form>
+
+
+      <ul>
+        {itemNodes}
+      </ul>
+
     </div>
   );
 }
